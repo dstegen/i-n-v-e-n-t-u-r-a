cf416fd31cd4b88e7ca6276c8cb323357e6a8eeb
Daniel <ds@danielstegen.de>
2020-03-15 15:36:45 +0100

    added asc and desc sorting, code cleanup

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,12 @@
 /*!
  * public/scripts.js
- * inventura
+ * i-n-v-e-n-t-u-r-a (https://github.com/dstegen/i-n-v-e-n-t-u-r-a)
  * Copyright 2020 Daniel Stegen <info@danielstegen.de>
- * Licensed under MIT
+ * Licensed under MIT (https://github.com/dstegen/i-n-v-e-n-t-u-r-a/blob/master/LICENSE)
  */
+
+
+// editing items
 
 let editObj = {};
 
@@ -19,6 +22,9 @@ function editItem (id) {
   $('#editModal').modal('toggle');
 }
 
+
+// filters
+
 function hideOthers (cat) {
   categories.forEach( item => {
     $('.'+item).hide();
@@ -31,6 +37,9 @@ function showAll () {
     $('.'+item).show();
   });
 }
+
+
+// full text search in items
 
 $(document).on('submit','form#searchItem',function(e){
   findMe($('#searchterm').val());
@@ -48,35 +57,45 @@ function findMe (text) {
   });
 }
 
+
+// table sorting
+
+let colums = $('#myTable')[0].rows[0].childElementCount-1;
+let directionList = [];
+for (let i=0; i<colums; i++) {
+  directionList.push('asc');
+};
+
 function sortTable(column) {
+  let direction = directionList[column];
+  if (direction === 'asc') {
+    directionList[column] = 'des';
+  } else {
+    directionList[column] = 'asc';
+  }
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("myTable");
   switching = true;
-  /*Make a loop that will continue until
-  no switching has been done:*/
   while (switching) {
-    //start by saying: no switching is done:
     switching = false;
     rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
     for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
       shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[column];
       y = rows[i + 1].getElementsByTagName("TD")[column];
-      //check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
+      if (direction == 'des') {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
       }
     }
     if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }

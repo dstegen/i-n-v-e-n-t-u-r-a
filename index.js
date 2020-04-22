329d1a09574b5f84a99009b5ca7027aad80704b9
Daniel <ds@danielstegen.de>
2020-04-22 19:20:52 +0200

    added port commandline parameter

--- index.js
+++ index.js
@@ -14,6 +14,16 @@ const router = require('./lib/router');
 // Name the process
 process.title = 'i-n-v-e-n-t-u-r-a';
 
-const server = new ServerDS('i-n-v-e-n-t-u-r-a');
+let port = 8080;
+
+try {
+	if (process.argv[2] && process.argv[2].includes('port')) {
+		port = Number(process.argv[2].split('=')[1]);
+	}
+} catch (e) {
+	console.log('ERROR reading port number from command line: '+e);
+}
+
+const server = new ServerDS('i-n-v-e-n-t-u-r-a', port);
 server.setCallback(router);
 server.startServer();
